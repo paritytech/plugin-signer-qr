9ba329f6e722dde7426d9c43e5a605b247608430
Jaco Greeff <jaco@parity.io>
2017-09-11 09:23:20 +0200

    Remove debugiing borders

--- src/request.js
+++ src/request.js
@@ -14,69 +14,55 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import TransactionPending from '@parity/ui/Signer/TransactionPending';
 
-import styles from './request.css';
+export default function Request ({ className, date, focus, gasLimit, id, isSending, netVersion, onConfirm, onReject, payload, signerStore, origin }) {
+  const transaction = payload.sendTransaction || payload.signTransaction;
 
-export default class Request extends Component {
-  static contextTypes = {
-    api: PropTypes.object.isRequired
-  };
-
-  static propTypes = {
-    className: PropTypes.string,
-    date: PropTypes.instanceOf(Date).isRequired,
-    focus: PropTypes.bool,
-    gasLimit: PropTypes.object.isRequired,
-    id: PropTypes.object.isRequired,
-    isSending: PropTypes.bool.isRequired,
-    netVersion: PropTypes.string.isRequired,
-    onConfirm: PropTypes.func.isRequired,
-    onReject: PropTypes.func.isRequired,
-    origin: PropTypes.object.isRequired,
-    payload: PropTypes.oneOfType([
-      PropTypes.shape({ decrypt: PropTypes.object.isRequired }),
-      PropTypes.shape({ sendTransaction: PropTypes.object.isRequired }),
-      PropTypes.shape({ sign: PropTypes.object.isRequired }),
-      PropTypes.shape({ signTransaction: PropTypes.object.isRequired })
-    ]).isRequired,
-    signerStore: PropTypes.object.isRequired
-  };
-
-  static isHandler (payload, account) {
-    const isTransaction = !!(payload.sendTransaction || payload.signTransaction);
-    const isExternalAccount = !!(account && account.external);
-
-    return isTransaction && isExternalAccount;
-  }
-
-  render () {
-    const { className, date, focus, gasLimit, id, isSending, netVersion, onConfirm, onReject, payload, signerStore, origin } = this.props;
-    const transaction = payload.sendTransaction || payload.signTransaction;
-
-    return (
-      <div className={ styles.body }>
-        <div className={ styles.hello }>
-          Hello from a signer plugin
-        </div>
-        <TransactionPending
-          className={ className }
-          date={ date }
-          focus={ focus }
-          gasLimit={ gasLimit }
-          id={ id }
-          isSending={ isSending }
-          netVersion={ netVersion }
-          onConfirm={ onConfirm }
-          onReject={ onReject }
-          origin={ origin }
-          signerStore={ signerStore }
-          transaction={ transaction }
-        />
-      </div>
-    );
-  }
+  return (
+    <TransactionPending
+      className={ className }
+      date={ date }
+      focus={ focus }
+      gasLimit={ gasLimit }
+      id={ id }
+      isSending={ isSending }
+      netVersion={ netVersion }
+      onConfirm={ onConfirm }
+      onReject={ onReject }
+      origin={ origin }
+      signerStore={ signerStore }
+      transaction={ transaction }
+    />
+  );
 }
+
+Request.propTypes = {
+  className: PropTypes.string,
+  date: PropTypes.instanceOf(Date).isRequired,
+  focus: PropTypes.bool,
+  gasLimit: PropTypes.object.isRequired,
+  id: PropTypes.object.isRequired,
+  isSending: PropTypes.bool.isRequired,
+  netVersion: PropTypes.string.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  onReject: PropTypes.func.isRequired,
+  origin: PropTypes.object.isRequired,
+  payload: PropTypes.oneOfType([
+    PropTypes.shape({ decrypt: PropTypes.object.isRequired }),
+    PropTypes.shape({ sendTransaction: PropTypes.object.isRequired }),
+    PropTypes.shape({ sign: PropTypes.object.isRequired }),
+    PropTypes.shape({ signTransaction: PropTypes.object.isRequired })
+  ]).isRequired,
+  signerStore: PropTypes.object.isRequired
+};
+
+Request.isHandler = function (payload, account) {
+  const isTransaction = !!(payload.sendTransaction || payload.signTransaction);
+  const isExternalAccount = !!(account && account.external);
+
+  return isTransaction && isExternalAccount;
+};
